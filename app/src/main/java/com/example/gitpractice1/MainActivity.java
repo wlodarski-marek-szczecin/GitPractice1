01e2874192da88a92019fd1d18360d4e3beea0ad
Wlodarski Marek <marek.wlodarski.szczecin@outlook.com>
2022-11-06 23:43:35 +0100

    Tabela z przesunięciem i spanem.

--- app/src/main/java/com/example/gitpractice1/MainActivity.java
+++ app/src/main/java/com/example/gitpractice1/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView napis = (TextView) findViewById(R.id.napis);
-        napis.setText("Witaj świecie");
+        napis.setText("Formularz w postaci tabeli");
     }
 }
