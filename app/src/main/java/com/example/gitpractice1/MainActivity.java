fcd428fe5b9eac074af95b938e1607a838cb3222
Wlodarski Marek <marek.wlodarski.szczecin@outlook.com>
2022-11-06 20:39:25 +0100

    Zmiana napisu wyświetlanego w TextView.

--- app/src/main/java/com/example/gitpractice1/MainActivity.java
+++ app/src/main/java/com/example/gitpractice1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gitpractice1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView napis = (TextView) findViewById(R.id.napis);
+        napis.setText("Witaj świecie");
     }
 }
